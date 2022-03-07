ff300ca39c70d96f7feed29bba7c5306ab8738df
RandxmStranger <dpaciorkowski0@gmail.com>
2022-03-07 11:22:29 +0000

    you can now score

--- server.py
+++ server.py
@@ -224,4 +224,4 @@ def send_leader():
     socketio.emit("sendleader", score, room = request.sid)
 
 if __name__ == "__main__":
-    socketio.run(app, debug = True)
+    socketio.run(app, debug = True, host="0.0.0.0", port=5000)
