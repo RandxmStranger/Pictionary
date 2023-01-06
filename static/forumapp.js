359ee0f2c87c9507052dfcae6821438daa2f37b9
RandxmStranger <dpaciorkowski0@gmail.com>
2023-01-06 11:25:09 +0000

    added functionality to forum buttons

--- static/forumapp.js
+++ static/forumapp.js
@@ -1,9 +1,17 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port); //This creates a socket connection with the flask server
 
-function makepost(){
+function new_post(){
     
 }
-
+function game(){ //This function takes the user to the forum route when they press the forum button on the page
+    location.href = "/"
+}
+function leaderboard(){ //This function takes the user to the forum route when they press the forum button on the page
+    location.href = "/leaderboard"
+}
+function logout(){ //This function takes the user to the forum route when they press the forum button on the page
+    location.href = "/logout"
+}
 socket.on('redirect', function(data) { //This function redirects the user to the address specified by the event, the server will send the request to redirect along with the address to redirect to any time it needs the user to be redirected
     window.location = data.url;
 });
