3b59a5281ebfc8d2d2be65e64989f29a6dfeb4d1
RandxmStranger <dpaciorkowski0@gmail.com>
2023-02-02 10:28:43 +0000

    fixed the navbar buttons on the newpost page

---
+++ static/newpostapp.js
@@ -0,0 +1,11 @@
+function game(){ //This function takes the user to the forum route when they press the forum button on the page
+    location.href = "/"
+}
+
+function leaderboard(){ //This function takes the user to the forum route when they press the forum button on the page
+    location.href = "/leaderboard"
+}
+
+function logout(){ //This function takes the user to the forum route when they press the forum button on the page
+    location.href = "/logout"
+}
